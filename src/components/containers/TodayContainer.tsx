03a8cc389170dcab2fa8f72b23f4927c996f3a20
ChukwuemekaAjaero <emeka.ajaero7@gmail.com>
2021-11-07 02:45:22 -0500

    WA-4: Using the default Roboto font

---
+++ src/components/containers/TodayContainer.tsx
@@ -0,0 +1,24 @@
+// -------------------- IMPORTS --------------------
+import React from "react";
+import styled from "styled-components";
+// -------------------- INTERFACES --------------------
+
+interface Props {
+}
+
+interface State {
+
+}
+
+// -------------------- STYLED COMPONENTS --------------------
+const Layout = styled.div`
+    color: #61dafb;
+`
+
+const TodayContainer = () => {
+    return(
+        <div></div>
+    )
+}
+
+export default TodayContainer;
