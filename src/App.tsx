f08525e15ebcacb48c726d55122f2e9eb326fb29
ChukwuemekaAjaero <emeka.ajaero7@gmail.com>
2022-01-02 06:47:04 -0500

    WA-3: Finished configuring the main dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {createContext, FunctionComponent, useContext, useMemo, useState} from "react";
+import {Grid, PaletteMode, responsiveFontSizes, Button, useTheme} from "@mui/material";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+import TodaySection from "./components/containers/TodaySection";
+import InformationSection from "./components/containers/InformationSection";
+import {getAppTheme} from "./styles/Theme";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// -------------------- INTERFACES --------------------
+
+// -------------------- THEMING --------------------
+const ColorModeContext = createContext({
+    toggleColorMode: () => {
+    }
+});
+
+
+// -------------------- COMPONENTS --------------------
+const App: FunctionComponent = () => {
+    const theme = useTheme();
+    const colorMode = useContext(ColorModeContext)
+    return (
+        <ThemeProvider theme={theme}>
+            <Grid container spacing={2} wrap={"wrap"}>
+                <Grid item xs={12} md={4} style={{backgroundColor: `${theme.palette.secondary.main}`}}>
+                    {/*<Button variant="contained" onClick={colorMode.toggleColorMode}>Test3</Button>*/}
+                    <TodaySection theme={theme}/>
+                </Grid>
+                <Grid item xs={12} md={8} style={{backgroundColor: `${theme.palette.primary.main}`}}>
+                    <InformationSection theme={theme}/>
+                </Grid>
+            </Grid>
+        </ThemeProvider>
+    );
 }
 
-export default App;
+const ToggleColorMode: FunctionComponent = () => {
+
+    const [mode, setMode] = useState<PaletteMode>('dark');
+    const colorMode = useMemo(() => ({
+            toggleColorMode: () => {
+                setMode((prevMode: PaletteMode) => prevMode === 'light' ? 'dark' : 'light',
+                );
+            },
+        })
+        , []);
+
+    let theme = useMemo(() => createTheme(getAppTheme(mode)), [mode]);
+    theme = responsiveFontSizes(theme);
+
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <App/>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    );
+}
+
+export default ToggleColorMode;
---
+++ src/styles/Theme.ts
@@ -0,0 +1,54 @@
+import {PaletteMode} from "@mui/material";
+
+//TODO: Set the colours for the light mode version.
+export const getAppTheme = (mode: PaletteMode) => ({
+    palette: {
+        mode,
+        ...(mode === 'dark' ? darkModeOptions : lightModeOptions)
+    },
+    typography: {
+        fontFamily: [
+            'Raleway'
+        ]
+    },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    textTransform:"none",
+                    borderRadius:0,
+                            color: mode === 'dark' ? '#E7E7EB': '#FFFFFF',
+                    backgroundColor: mode === 'dark' ? '#6E707A': '#FFFFFF'
+                }
+            }
+        }
+    }
+
+})
+
+
+const darkModeOptions = {
+    primary: {
+        main: '#1E213A',
+        contrastText: "#fff"
+    },
+    secondary: {
+        main: '#100E1D',
+        contrastText: "#fff"
+    },
+    text: {
+        primary: '#E7E7EB',
+        secondary: '#A09FB1',
+        tertiary: '#88869D'
+    }
+}
+
+const lightModeOptions = {
+    primary: {
+        main: '#757DE8'
+    },
+    secondary: {
+         main: '#FF7961'
+    }
+}
+
